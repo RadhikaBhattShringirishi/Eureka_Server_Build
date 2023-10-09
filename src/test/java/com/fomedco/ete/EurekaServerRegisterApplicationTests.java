b81bb9fef4408e97816cdfcd4ac5bc8b6071d838
radhi <radhikabhatt.ec@gmail.com>
2023-10-09 09:33:32 +0300

    Eureka Server Build commit

---
+++ src/test/java/com/fomedco/ete/EurekaServerRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fomedco.ete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
